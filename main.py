746d65f52ee1109b11064a902b8cf2a3a85ee97d
ChaMinsu <hong@gil.dong>
2026-05-09 00:02:30 +0900

    CORS 설정 수정
    Signed-off-by: ChaMinsu <hong@gil.dong>

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://*.vercel.app",
+        "https://khuthonbackend-production.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
